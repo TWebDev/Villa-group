6cc1ce34b72f83ed618ebb547d1ad4f7cc06a554
PaolaVazquez <paola.vazra98@gmail.com>
2021-06-09 13:27:42 -0500

    sitio

---
+++ BVM/BVM-Landing/Cambios/index.php
@@ -0,0 +1,326 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Facebook Pixel Code -->
+    <script>
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window,document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '432430217457490'); 
+      fbq('track', 'PageView');
+      fbq('track', 'Lead');
+    </script>
+    <noscript>
+      <img height="1" width="1" src="https://www.facebook.com/tr?id=432430217457490&ev=PageView
+      &noscript=1"/>
+    </noscript>
+    <!-- End Facebook Pixel Code -->
+    <title> Beach Vacations Mexico </title>
+   
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+      (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+      m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+      (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+      ym(68955691, "init", {
+          clickmap:true,
+          trackLinks:true,
+          accurateTrackBounce:true,
+          webvisor:true
+      });
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/68955691" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
+
+
+    <!-- CSS -->
+    <link href="/Content/landings/bva/main/css/responsive.css" rel="stylesheet">
+    <link href="/Content/landings/bva/main/css/style.css" rel="stylesheet">
+    <link href="/Content/landings/bva/main/css/fonts/stylesheet.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+    <!-- Video Hero -->
+    <div class="header">
+      
+      <div id="overlay">
+        <!-- Large Device Navigation -->
+        <div id="navbar">
+          <div id="logoTop">
+            <a id="logo" href="/"><img src="/Content/landings/bva/main/img/BVM_Web Asses-02.png"  alt="Beach Vacations Mexico"></a>
+          </div>
+          <div id="links">
+            <ul>
+              <li> <a href="https://www.facebook.com/beachvacationsmexico/">
+                <i class="fa fa-facebook-official fa-2x" aria-hidden="true"></i>
+              </a></li>
+              <li>    
+                <a href="https://www.instagram.com/beachvacationsmexico/">
+                  <i class="fa fa-instagram fa-2x" aria-hidden="true"></i>
+                </a></li>
+                <li>
+                  <a href="https://twitter.com/BeachVacationsM"><img src="/Content/landings/bva/main/img/BVM_Web Asses_twitter.png" alt="Twitter" style="max-width: 40px; vertical-align: sub;" ></a>
+                </li>
+              <li><a href="/puerto-vallarta/">Puerto Vallarta</a></li>
+              <li><a href="/cancun/">Cancun</a></li>
+              <li><a href="/cabo-san-lucas/">Cabo</a></li>
+              <li><a href="/loreto/">Loreto</a></li>
+              <li id="users"><a href="#book">Book Now</a></li>
+            </ul>
+          </div>
+        </div>
+
+        <!-- Small Device Navigation -->
+        <div id="mobile-nav">
+          <a id="menu-toggle" href="#" class="btn btn-dark btn-lg toggle">
+            <i style="color: #EAB13C" class="fa fa-bars" aria-hidden="true"></i>
+          </a>
+          <nav id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+              <a id="menu-close" href="#" class="btn btn-light btn-lg pull-right toggle">
+                <i style="color: #EAB13C" class="fa fa-times" aria-hidden="true"></i>
+              </a>
+              <li class="sidebar-brand">
+                <a href="#book">Book Now</a>
+              </li>
+              <li>
+                <a href="/puerto-vallarta/">Puerto Vallarta</a>
+              </li>
+              <li>
+                <a href="/cancun/">Cancun</a>
+              </li>
+              <li>
+                <a href="/cabo-san-lucas/">Cabo</a>
+              </li>
+              <li>
+                <a href="/loreto/">Loreto</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+        <!-- Banner Message Goes Here! -->
+        <div id="messagebox" class="text-center">
+          <h2>Welcome to </h2>
+          <h1>Infinity!</h1>
+          <p>Welcome to this wild ride around the gorgeous beaches in Mexico! <br> Pack your bags, put your sunglasses on, and travel with us!</p>
+          <a href="#overview" class="btn btn-lg btn-primary">Book Now</a>
+          <div style="position: relative; margin-top: 25px;">
+            <img src="/Content/landings/bva/main/img/TIL_Web Assets-05.svg" />
+          </div>
+        </div>
+      <!-- Closing tags -->
+      </div>
+    </div>
+
+    <!-- Overview Section -->
+    <div id="overview" class="container">
+      <div class="text-center" style="width: 100%;">
+        <h2>Our</h2>
+        <h1>Destinations</h1>
+        <div class="col-sm-3 sm-dev-spacing" style="padding: 5px">
+          <div id="PV">
+            <div><h2 style="color: #EAB13C; font-size:3.8em ;">Puerto Vallarta</h2></div>
+          </div>
+          <div class="img-des">
+            <p></p>
+          </div>
+        </div>
+
+        <div class="col-sm-3 sm-dev-spacing" style="padding: 5px">
+          <div id="Cancun">
+            <div><h2 style="color: #EAB13C;">Cancun</h2></div>
+          </div>
+          <div class="img-des">
+            <p></p>
+          </div>
+        </div>
+
+        <div class="col-sm-3 sm-dev-spacing" style="padding: 5px">
+          <div id="Cabo">
+            <div><h2 style="color: #EAB13C;">Cabo</h2></div>
+          </div>
+          <div class="img-des">
+            <p></p>
+          </div>
+        </div>
+        <div class="col-sm-3 sm-dev-spacing" style="padding: 5px">
+          <div id="Loreto">
+            <div><h2 style="color: #EAB13C;">Loreto</h2></div>
+          </div>
+          <div class="img-des">
+            <p></p>
+          </div>
+        </div>
+        <!-- Closing tags -->
+      </div>
+    </div>
+    <!--Formulario-->
+    <div id="book" class="formulario">
+      <div class="container " >
+        <div class="row">
+            <form action="https://gmail.us3.list-manage.com/subscribe/post?u=794ab740eeeedda6c0bd0da31&amp;id=84a880e7d1" class="form-1 validate"  method="post"  id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank" novalidate>
+              <h1>Register</h1>
+              <div class="mc-field-group">
+                <label for="mce-FNAME">First Name</label>
+                <input type="text" value="" name="FNAME" id="mce-FNAME" required="required" aria-required="true" pattern="^([- \w\d\u00c0-\u024f]+)$" aria-invalid="true" class=" mob form-control form-control">
+              </div>
+              <div class="mc-field-group">
+                <label for="mce-LNAME">Last Name</label>
+                <input type="text" value="" name="LNAME" id="mce-LNAME" required="required" aria-required="true" pattern="^([- \w\d\u00c0-\u024f]+)$" aria-invalid="true" class=" mob form-control form-control">
+              </div>
+              <div class="mc-field-group">
+                <label for="mce-EMAIL">Email</label>
+                <input type="email" class="mob form-control form-control" value="" name="EMAIL"  id="mce-EMAIL" required="required" aria-required="true" pattern="^(([-\w\d]+)(\.[-\w\d]+)*@([-\w\d]+)(\.[-\w\d]+)*(\.([a-zA-Z]{2,5}|[\d]{1,3})){1,2})$" aria-invalid="true">
+              </div>
+              <div class="mc-field-group size1of2">
+                <label for="mce-PHONE">Phone</label>
+                <input type="text" name="PHONE" value="" id="mce-PHONE" required="required" aria-required="true" pattern="^\d{10}$" aria-invalid="true" class="mob form-control form-control">
+              </div>
+              <div class="mc-field-group">
+                <label for="mce-PREF_DATES">When are you planning to travel?</label>
+                <select name="PREF_DATES" class="" id="mce-PREF_DATES">
+                  <option value=""></option>
+                  <option value="January - April">January - April</option>
+                  <option value="May - August">May - August</option>
+                  <option value="September - December">September - December</option>
+                </select>
+              </div>
+              <div class="mc-field-group">
+                <label for="mce-CALL_AT">What's the best time to call you?</label>
+                <select name="CALL_AT" class="" id="mce-CALL_AT">
+                  <option value=""> </option>
+                  <option value="9:00 am - 12:00 pm">9:00 am - 12:00 pm</option>
+                  <option value="12:00 pm - 3:00 pm">12:00 pm - 3:00 pm</option>
+                  <option value="3:00 pm - 6:00 pm">3:00 pm - 6:00 pm</option> 
+                </select>
+              </div>
+
+              <div class="mc-field-group input-group" style="display: none;">
+                <strong>Landing campaign </strong>
+                <ul>
+                  <li><input type="checkbox" value="1" name="group[27281][1]" id="mce-group[27281]-27281-0-main" checked><label for="mce-group[27281]-27281-0">Landing page</label></li>
+                  <li><input type="checkbox" value="2" name="group[27281][2]" id="mce-group[27281]-27281-1-main"><label for="mce-group[27281]-27281-1">Yoga/Golf</label></li>
+                </ul>
+              </div>
+                <div id="mce-responses" class="clear">
+                  <div class="response" id="mce-error-response" style="display:none"></div>
+                  <div class="response" id="mce-success-response" style="display:none"></div>
+              <button class="btn btn-lg btn-primary btn-form center"  name="subscribe"  type="submit" id="mc-embedded-subscribe-mob"  onclick="sendToEplat()">Book now</button>
+                </div>
+
+
+            </form>
+          </div>
+        </div>
+    </div>
+
+
+    <div class="cookies-box">
+      <div class="container-cookies">
+          <div class="parrafo">
+              <p>We use our own and third-party cookies to analyze our services and show you advertising related to your preferences based on a profile created from your browsing habits 
+                (for example, pages visited). You can obtain more information by consulting the <a href="Cookies.html">Cookies Policy</a>  and configure your preferences below. </p>
+          </div>
+          <div class="botones">
+              <button>Acept</button>
+              <button id="close">Close</button>
+          </div>
+      </div>
+  </div>
+
+  <script>
+      const btn_close = document.querySelector('#close')
+      const cont_cookies = document.querySelector('.cookies-box')
+      
+      btn_close.addEventListener('click', () => {
+        cont_cookies.style.bottom = '-200px'  
+      })
+  </script>
+
+    <!-- Footer -->
+    <div id="footer" class="container-fluid">
+      <div class="row rowspacing text-center">
+        <!-- Logo -->
+        <div class="col-sm-3"><a href="index.html"><img src="/Content/landings/bva/main/img/BVM_Web Asses-01.png" alt="Beach Vacations Mexico"></a> </div>       
+
+        <!-- Social Media -->
+        <div class="col-xs-12 col-sm-3 sm-dev-spacing pull-right">
+
+            <a href="https://www.facebook.com/beachvacationsmexico/s">
+              <i class="fa fa-facebook-official fa-2x" aria-hidden="true"></i>
+            </a> 
+              <a href="https://www.instagram.com/beachvacationsmexico/">
+                <i class="fa fa-instagram fa-2x" aria-hidden="true"></i>
+              </a>
+
+              <a href="https://twitter.com/BeachVacationsM"><img src="/Content/landings/bva/main/img/BVM_Web Asses_twitter.png" alt="Twitter" style="max-width: 35px; vertical-align: sub;" ></a>
+
+         
+
+        </div>
+      </div>
+    </div>
+    
+    <script type="text/javascript">
+      function checkCampaign () {
+        document.getElementById("mce-group[27281]-27281-1-main").checked = true;
+      }
+    </script>
+    <script src="/Content/landings/bva/main/js/jquery.js"></script>
+    <script src="/Content/landings/bva/main/js/application.js"></script>
+    <script src="/Content/landings/bva/main/js/formValidation.js"></script>
+
+
+    <?php 
+
+$value = 'something from somewhere';
+
+setcookie("TestCookie", $value);
+setcookie("TestCookie", $value, time()+3600);  /* expire in 1 hour */
+setcookie("TestCookie", $value, time()+3600, "/~rasmus/", "beachvacations.com.mx", 1);
+
+
+
+// Another way to debug/test is to view all cookies
+//print_r($_COOKIE);
+
+
+$arr_cookie_options = array (
+    'expires' => time() + 60*60*24*30,
+    'path' => '/',
+    'domain' => '.example.com', // leading dot for compatibility or use subdomain
+    'secure' => true,     // or false
+    'httponly' => true,    // or false
+    'samesite' => 'Lax' // None || Lax  || Strict
+    );
+setcookie('TestCookie', 'The Cookie Value', $arr_cookie_options);  
+
+//print_r($_COOKIE);
+?>
+
+
+
+        <!-- Twitter universal website tag code -->
+        <script>
+      !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+      },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='//static.ads-twitter.com/uwt.js',
+      a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+      // Insert Twitter Pixel ID and Standard Event data below
+      twq('init','o4vo1');
+      twq('track','PageView');
+      </script>
+      <!-- End Twitter universal website tag code -->
+  </body>
+
+</html>
