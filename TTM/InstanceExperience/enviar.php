8abe7b6370285b1bbb08cc7ad2b80bde6b46a4d6
PaolaVazquez <paola.vazra98@gmail.com>
2021-06-09 12:47:47 -0500

    TTM

---
+++ TTM/InstanceExperience/enviar.php
@@ -0,0 +1,23 @@
+<?php
+ //Llamada a campos
+ $fname = $_POST['fname'];
+ $lname = $_POST['lname'];
+ $phone = $_POST['phone'];
+ $email = $_POST['email'];
+
+ //Datos hacia el correo
+ $destinatario = "paola.vazra98@gmail.com";
+ $asunto = "Lead Instance Experience";
+
+ $carta = "De: $fname \n ";
+ $carta .= "Email: $email \n";
+ $carta .= "Phone: $phone ";
+
+//Enviando mensaje
+mail($destinatario, $asunto, $carta);
+
+
+
+
+
+ ?>
